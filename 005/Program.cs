47cc60f80802f21fe443edc4c898b303843d2183
Sadak <s.sadak02@gmail.com>
2023-05-29 17:34:13 +0600

    Массив bool больше 1 или нет

---
+++ 005/Program.cs
@@ -0,0 +1,36 @@
+﻿// необходимо создать функцию булевого типа, которая проверяет 
+// находится ли в массиве единиц больше чем нулей
+
+
+void Arr(int[] Array)
+{
+    for (int i = 0; i<Array.Length; i++)
+    {
+         Array[i] = new Random().Next(0, 2);
+    }
+}
+
+bool OneZero(int[] Array)
+{
+    int count = 0;
+    for (int i = 0; i<Array.Length; i++)
+    {
+        if (Array[i] == 1)
+            count++;
+    }
+
+    bool flag = false;
+    if (count>Array.Length/2)
+        flag = true;
+    return flag;
+}
+
+
+Console.Clear();
+int[] Array = new int[8];
+Arr(Array);
+foreach (var i in Array)    
+    System.Console.Write(i+" ");
+
+
+System.Console.WriteLine(OneZero(Array));
