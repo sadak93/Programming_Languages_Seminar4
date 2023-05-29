1a5eee674e755f1d8bf1692b6da3edad083c06ab
Sadak <s.sadak02@gmail.com>
2023-05-29 17:30:52 +0600

    Сумма чисел до N

---
+++ 001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Summa(int num)
+{
+    int result = 0;
+    for (int i=1; i<=num; i++)
+        result+=i;
+    return result;
+}
+
+Console.Clear();
+System.Console.WriteLine("Ввести натуральное целое число ");
+int num = Convert.ToInt32 (Console.ReadLine());
+System.Console.WriteLine($"Сумма чисел от 1 до {num} равна {Summa(num)}");
